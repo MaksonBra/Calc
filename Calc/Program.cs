2117bc1176baae352071c307fd1e6c0949da8a38
Maksim <m.bratenev@gmail.com>
2024-08-28 22:29:40 +0500

    added dictionary with coin denomination and amount, as well as input of change amount from the keyboard

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -7,6 +7,32 @@ public class Program
 {
     static void Main()
     {
+        var cashRegister = new Dictionary<int, int>
+        {
+            { 100, 2 }, { 50, 3 }, { 10, 5 }, { 5, 6 }, { 2, 3 }, { 1, 25 } // Словарик с монетами в кассе
+        };
+
+        Console.WriteLine("Введите сумму сдачи:");
+        if (!int.TryParse(Console.ReadLine(), out int changeAmount) || changeAmount <= 0)
+        {
+            Console.WriteLine("Некорректное значение");
+            return;
+        }
+
+        var change = GetChange(cashRegister, changeAmount);
+
+        if (change != null)
+        {
+            Console.WriteLine("Можно выдать сдачу:");
+            foreach (var coin in change)
+            {
+                Console.WriteLine($"{coin.Key}: {coin.Value}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Невозможно выдать сдачу - оплата кредитной картой");
+        }
     }
     public static Dictionary<int, int>? GetChange(Dictionary<int, int> cashRegister, int changeAmount)
     {
