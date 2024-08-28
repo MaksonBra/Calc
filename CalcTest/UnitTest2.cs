ac090056fb058d013a9ae9b49f19338a413d7ca7
Maksim <m.bratenev@gmail.com>
2024-08-28 21:10:52 +0500

    added a test to check if the change cant be issued

--- CalcTest/UnitTest2.cs
+++ CalcTest/UnitTest2.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System;
 
 namespace CalcTest
 {
-    internal class Class1
+    [TestClass]
+    public class UnitTest2
     {
+        [TestMethod]
+        static void Test2()
+        {
+            // Этот тест проверяет ситуацию, когда сдачу выдать невозможно 
+            // В кассе есть одна монета номиналом 5  и две монеты номиналом 1, а сумма сдачи — 7 
+            // Ожидается, что функция вернёт null, так как невозможно собрать нужную сумму из имеющихся монет
+            var cashRegister = new Dictionary<int, int>
+            {
+                { 100, 0 },
+                { 50, 0 },
+                { 10, 0 },
+                { 5, 1 },   // 1 по 5 
+                { 2, 0 },
+                { 1, 2 }    // 2 по 1
+            };
+            int changeAmount = 7;
+
+            // Ожидаемый результат: null
+            var expectedChange = null as Dictionary<int, int>;
+
+            // Вызов функции
+            var actualChange = GetChange(cashRegister, changeAmount);
+
+            // Проверка результата
+            Assert.AreEqual(expectedChange, actualChange);
+        }
     }
 }
