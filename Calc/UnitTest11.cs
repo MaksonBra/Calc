de02dfdffa86fe5205f023de28a93f2e6ae308b7
Maksim <Maksim@MAKSON>
2024-08-28 16:39:51 +0500

    Добавьте файлы проекта.

---
+++ Calc/UnitTest11.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Calc
+{
+    [TestClass]
+    public class UnitTest11
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
